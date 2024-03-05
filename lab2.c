a22fc034c0b1164cd4fc9f4b831692a1e966c1bc
Marin Ionescu <marinionescu@gmail.com>
2024-03-05 15:43:16 +0200

    program modificat

--- lab2.c
+++ lab2.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 
 int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+    while(x)
+    {
+        x = x - 2;
+        x = function(x);
+    }
+    return x;
 }
 
 int main(){
-	int a=30, i=0;
+	int a = 30, i = 0;
 
-	while (a>0){
-		a=function(a);
+	while (a > 0){
+		a = function(a);
 		i++;
 	}
 	printf("%d", i);
